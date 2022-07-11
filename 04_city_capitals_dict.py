8930fae3f1c98c57a7d73b4fc0043a0f18dd226f
Vijay Vardhan Vasamsetti <106310705+Vijay-159@users.noreply.github.com>
2022-07-11 20:52:25 +0530

    Update 04_city_capitals_dict.py

--- 04_city_capitals_dict.py
+++ 04_city_capitals_dict.py
@@ -2,15 +2,10 @@
 
 '''
 You will get a dictionary with a state as key and its capital as value, 👇 see the below example
-
 capitals = {"Karnataka":"Bangalore", "Telangana" : "Hyderabad"}
-
 Now your task is to bring a list which should contain like the below one
-
 ["Karnataka -> Bangalore", "Telangana -> Hyderabad"]
-
 Return this list
-
 '''
 
 import unittest
@@ -18,7 +13,12 @@ import unittest
 def capital_dict(d1):
   str_lst = []
   # write your code here
+def capital_dict(d1):
+  str_lst = []
+  for i in d1.items():
+      str_lst.append(i[0]+' -> '+i[1])
 
+  return str_lst
   return str_lst
 
 
@@ -27,16 +27,16 @@ def capital_dict(d1):
 class Dict_to_list(unittest.TestCase):
   def test_01(self):
     d1 = {"Andhra": "Amaravati", "Madhyapradesh" : "Bhopal", "Maharastra" : "Mumbai" }
-    
+
     output = ["Andhra -> Amaravati", "Madhyapradesh -> Bhopal", "Maharastra -> Mumbai"]
-    
+
     self.assertEqual(capital_dict(d1), output)
 
   def test_02(self):
     d1 = {"J&K": "Srinagar", "Rajastan" : "Jaipur", "Gujarat" : "Gandhinagar" }
-    
+
     output = ["J&K -> Srinagar", "Rajastan -> Jaipur", "Gujarat -> Gandhinagar"]
-    
+
     self.assertEqual(capital_dict(d1), output)
 
 if __name__ == '__main__':
